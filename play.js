90f921af9ea429239720777d050d212c74ad902a
Gabriel Silva <j.gabriel.888@gmail.com>
2021-03-10 18:57:20 +0000

    snake game

--- play.js
+++ play.js
@@ -1,13 +1,7 @@
 const {connect} = require('./client');
-console.log('Connecting ...');
+
 connect();
 
 
-const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
 
-console.log('Connecting ...');
-connect();
