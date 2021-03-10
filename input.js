70116aad620df464e5578c523dfed53c4f726620
Gabriel Silva <j.gabriel.888@gmail.com>
2021-03-10 20:33:11 +0000

    snake game

---
+++ input.js
@@ -0,0 +1,33 @@
+const {connect} = require("./client");
+
+let connection;
+
+const handleUserInput = (inputFromKey) => {
+  if (inputFromKey === '\u0003') {
+    process.exit();
+  }
+  if (inputFromKey === 'w') {
+    connection.write('Move: up')
+  }
+  if (inputFromKey === 'a') {
+    connection.write('Move: left')
+  }
+  if (inputFromKey === 's') {
+    connection.write('Move: down')
+  }
+  if (inputFromKey === 'd') {
+    connection.write('Move: right')
+  }
+}
+
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+module.exports = {setupInput}
--- play.js
+++ play.js
@@ -1,21 +1,10 @@
 const {connect} = require('./client');
+const {setupInput} = require('./input')
 
-const handleUserInput = (inputFromKey) => {
-  if (inputFromKey === '\u0003') {
-    process.exit();
-  }
-  console.log("testeeee")
-}
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-}
+console.log('Connecting ...');
 
 
-connect();
-setupInput();
+setupInput(connect());
+
+
+
